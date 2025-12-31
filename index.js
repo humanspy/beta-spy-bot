be856bf7ae91b9c4177cab63366e4560076d192f
humanspy <102175536+humanspy@users.noreply.github.com>
2025-12-31 02:33:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fs from "fs/promises";
 import bcrypt from "bcrypt";
 import { execSync } from "child_process";
 import { existsSync } from "fs";
-import "./server.js";
+import "./web/server.js";
 
 
 import {
