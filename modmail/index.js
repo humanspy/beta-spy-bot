3a0c20004bad361a57344f4306c0a14e223321a2
humanspy <102175536+humanspy@users.noreply.github.com>
2026-01-02 01:19:41 +0100

    Update index.js

--- modmail/index.js
+++ modmail/index.js
@@ -7,3 +7,13 @@ export async function handleModmail(interaction) {
   // Let the commands router decide
   return handleModmailCommands(interaction);
 }
+
+import {
+  handleModmailDM,
+  handleModmailThreadMessage,
+} from "./dmhandler.js";
+
+client.on("messageCreate", async message => {
+  await handleModmailDM(message, client);
+  await handleModmailThreadMessage(message);
+});
